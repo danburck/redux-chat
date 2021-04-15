c578b4817939918dafd3ad128f7797f11d30958a
danburck <daniel.m.burckhardt@gmail.com>
2021-04-15 14:40:39 +0200

    add MessageForm Container

---
+++ src/containers/message_form.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+};
+
+export default MessageForm;
